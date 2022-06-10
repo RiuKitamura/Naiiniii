cae4f747a9937c7af42950128f77dc9f15440341
Habib Akbar <habibakbar360@gmail.com>
2022-06-10 14:18:32 +0700

    Add framework

---
+++ Naiiniii/Naiiniii/Naiiniii.h
@@ -0,0 +1,18 @@
+//
+//  Naiiniii.h
+//  Naiiniii
+//
+//  Created by M Habib Ali Akbar on 10/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Naiiniii.
+FOUNDATION_EXPORT double NaiiniiiVersionNumber;
+
+//! Project version string for Naiiniii.
+FOUNDATION_EXPORT const unsigned char NaiiniiiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Naiiniii/PublicHeader.h>
+
+
